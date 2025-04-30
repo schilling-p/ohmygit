e58d5f3433c675a772908271e6e82dcf14dfb415
schilling-p <paulschilling@posteo.de>
2025-04-30 11:31:53 +0200

    debug the login function in the backend

--- backend/application/src/user/login.rs
+++ backend/application/src/user/login.rs
@@ -17,11 +17,9 @@ pub struct LoginResponse {
 pub async fn login_user(pool: State<DbPool>, Json(login_request): Json<LoginRequest>) -> Result<Json<LoginResponse>, AppError> {
     // TODO: don't do clone() , requires new signature of find_user_by_email()
     match find_user_by_email(&pool, login_request.email.clone()).await {
-        Ok(_) => Err(AppError::EmailAlreadyExists),
-        Err(_) => {
-            let user = find_user_by_email(&pool, login_request.email).await?;
+        Err(_) => Err(AppError::EmailAlreadyExists),
+        Ok(user) => {
             verify_password(&login_request.password, &user.hashed_pw)?;
-
             Ok(Json(LoginResponse { message: "Login successful".to_string() }))
         },
     }
