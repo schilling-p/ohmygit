0e83b006764114e172b509f35e724ea051ba3513
schilling-p <paulschilling@posteo.de>
2025-05-23 17:38:20 +0200

    enable login via email and username

--- backend/application/src/user/login.rs
+++ backend/application/src/user/login.rs
@@ -10,23 +10,32 @@ use tower_sessions::{Session};
 use domain::request::auth::{LoginRequest, UserIdentifier};
 use domain::response::auth::LoginResponse;
 use domain::ApiResponse;
+use domain::models::User;
 use infrastructure::diesel::DbPool;
 
 #[debug_handler]
-pub async fn login_user(session: Session, pool: State<DbPool>, Json(login_request): Json<LoginRequest>) -> Result<ApiResponse, AppError> {
-    debug!("login request with: {:?} and {:?}", login_request.email, login_request.password);
-    let user = retrieve_user_from_db(&pool, UserIdentifier::Email(login_request.email)).await?.0;
-    verify_password(&login_request.password, &user.hashed_pw)?;
+pub async fn user_login_handler(session: Session, pool: State<DbPool>, Json(login_request): Json<LoginRequest>) -> Result<ApiResponse, AppError> {
+    
+    match login_user(pool, Json(login_request)).await {
+        Ok(user) => {
+            session.insert("username", user.username.clone()).await?;
+            session.insert("user_email", user.email.clone()).await?;
+            
+            Ok(ApiResponse::Login(LoginResponse {
+                message: "login_successful",
+                // TODO: remove for production, is not needed anymore
+                user_email: user.email.clone(),
+                username: user.username.clone(),
+            }))
+        }
+        Err(e) => Err(e),
+    }    
+}
 
-    session.insert("username", user.username.clone()).await?;
-    let inserted_username: String = session.get("username").await?.unwrap_or("no username found".to_string());
-    debug!("username cookie set to: {}", inserted_username);
-
-    session.insert("user_email", user.email.clone()).await?;
-
-    Ok(ApiResponse::Login(LoginResponse {
-        message: "login_successful",
-        user_email: user.email,
-        username: user.username,
-    }))
+pub async fn login_user(pool: State<DbPool>, Json(login_request): Json<LoginRequest>) -> Result<Json<User>, AppError> {
+    let user = retrieve_user_from_db(&pool, login_request.identifier).await?.0;
+    match verify_password(&login_request.password, &user.hashed_pw) {
+        Ok(_) => Ok(Json(user)),
+        Err(_) => Err(AppError::Unauthorized),
+    }   
 }
