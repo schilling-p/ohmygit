dbd55863d967e9168e2c6dd180dd76135ebea715
schilling-p <paulschilling@posteo.de>
2025-06-07 11:30:32 +0200

    repo name validation with regex

--- backend/application/src/repository/create.rs
+++ backend/application/src/repository/create.rs
@@ -3,6 +3,9 @@ use axum::response::{IntoResponse, Redirect};
 use axum_macros::debug_handler;
 use axum::Json;
 use tower_sessions::Session;
+use regex::Regex;
+use std::sync::LazyLock;
+
 use domain::request::auth::UserIdentifier;
 use crate::user::read::retrieve_user_from_db;
 use domain::request::repository::CreateRepoRequest;
@@ -11,7 +14,6 @@ use infrastructure::diesel::DbPool;
 
 #[debug_handler]
 pub async fn create_repository(State(pool): State<DbPool>, session: Session, Json(create_repo_request): Json<CreateRepoRequest>) -> Result<impl IntoResponse, AppError> {
-    // validate the repo name
     // ensure the repo does not already exist
     // build repo path
     // create a user folder in /repos if missing
@@ -19,13 +21,25 @@ pub async fn create_repository(State(pool): State<DbPool>, session: Session, Jso
     // insert repository metadata into the database
     // create a new struct for the database insertion
     
+    if !is_valid_repo_name(&create_repo_request.repository_name) {
+        return Err(AppError::BadRequest("Invalid name for a repository".to_string()))
+    }
+
     let username: Option<String> = session.get("username").await?;
     if let Some(username) = username {
         let user = retrieve_user_from_db(&pool, UserIdentifier::Username(username)).await?;
         Ok(Redirect::to("/dashboard"))
-        
+
     } else {
         Err(AppError::Unauthorized)
     }
-    
+
+}
+
+static REPO_NAME_REGEX: LazyLock<Regex> = LazyLock::new( || {
+    Regex::new(r"^[a-zA-Z0-9][a-zA-Z0-9_-]{0,49}$").unwrap()
+});
+
+fn is_valid_repo_name(repo_name: &str) -> bool {
+    REPO_NAME_REGEX.is_match(repo_name)
 }
