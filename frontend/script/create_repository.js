fc43501b5eed404bc79c62dae248ce9d2737c0ac
schilling-p <paulschilling@posteo.de>
2025-06-07 13:56:29 +0200

    remove not needed block styles

--- frontend/script/create_repository.js
+++ frontend/script/create_repository.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Create repository page loaded.");
     console.log(repositories);
+    const form = document.getElementById("create-repository-form");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const repositoryName = document.getElementById("repository-name").textContent;
+        const repositoryDescription = document.getElementById("repository-description").textContent;
+        const isPublic = true;
+    })
 });
